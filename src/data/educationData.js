c5d00514108716a07c603f801148f00db5e170c0
Arpit200811 <arpitgupta9610@gmail.com>
2025-10-06 10:20:36 +0530

    addedd successfull

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -8,14 +8,14 @@ export const educationData = [
     },
     {
         id: 2,
-        institution: 'Sant Ravidas Government Polytechnic, Chakiya, Chandauli, Uttar Pradesh',
-        course: 'Diploma in Computer Science and Engineering (BTEUP)',
+        institution: 'SRGP, Chakiya, Chandauli, Uttar Pradesh',
+        course: 'Diploma in CSE (BTEUP)',
         startYear: '2021',
         endYear: '2024'
     },
     {
         id: 3,
-        institution: 'Dr. Savita Memorial Global Academy, Chunar, Mirzapur, Uttar Pradesh',
+        institution: 'Dr. S.M.G.A, Chunar, Mirzapur, Uttar Pradesh',
         course: 'Higher Secondary Education (10+2)',
         startYear: '2019',
         endYear: '2021'
