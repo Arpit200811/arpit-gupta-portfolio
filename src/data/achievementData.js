c64003e95a10c23d12afb39ef476b031ee4533f9
Arpit200811 <arpitgupta9610@gmail.com>
2025-10-09 14:44:07 +0530

    addedd successfull

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -7,7 +7,7 @@ export const achievementData = {
             details : 'Awarded for outstanding performance during the summer internship, demonstrating exceptional skills, dedication, and initiative in cloud technology projects.',
             date : 'Aug 20, 2023',
             field : 'Summer Intern',
-            image : 'https://images.unsplash.com/photo-1590608897129-79bda5cda2c5?ixlib=rb-4.0.3&auto=format&fit=crop&w=687&q=80'
+            image : 'https://th.bing.com/th/id/OIP.ZjODxeybe48YneGF_PCT_gHaEU?o=7&cb=12rm=3&rs=1&pid=ImgDetMain&o=7&rm=3'
         },
         {
             id : 2,
@@ -15,15 +15,15 @@ export const achievementData = {
             details : 'Successfully completed hands-on web development projects, acquiring practical skills in frontend and backend technologies, and demonstrating readiness for professional roles.',
             date : 'May 20, 2024',
             field : 'Junior Web Developer',
-            image : 'https://images.unsplash.com/photo-1551836022-d5d88e9218df?ixlib=rb-4.0.3&auto=format&fit=crop&w=1074&q=80'
+            image : 'https://media.istockphoto.com/vectors/internship-concept-vector-illustration-banner-with-keywords-and-icons-vector-id1211151590?k=20&m=1211151590&s=612x612&w=0&h=J1Y8udpCVsl9GoQifs2IVs8-DL7hZ3t_tEwNMNuTV4Q='
         },
         {
             id : 3,
             title : 'Best Employee of the session',
             details : 'Recognized as the best employee for consistently delivering high-quality software solutions, optimizing workflows, and contributing significantly to team success.',
             date : 'Aug 03, 2025',
-            field : 'Software Engineering',
-            image : 'https://images.unsplash.com/photo-1581091215367-5b6e6d7b1e69?ixlib=rb-4.0.3&auto=format&fit=crop&w=1050&q=80'
+            field : 'Full Stack Developer',
+            image : 'https://img.freepik.com/premium-vector/best-employee-award-design_597133-1116.jpg'
         }
     ]
 }
--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -2,6 +2,5 @@ export const contactsData = {
     email: 'arpitgupta9610@gmail.com',
     phone: '+91 6386373577',
     address: 'Vikas Nagar Lucknow Uttar Pradesh 226022',
-
-    sheetAPI: 'https://arpit-portfolio-server.onrender.com/api/contact'
+    sheetAPI: 'http://localhost:5000/api/contact'
 }
